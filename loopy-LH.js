3c08f27efc7d03feffb5a2d99a1da5c4771abe3f
Kent Gulka <kentgulka@modindsoft.com>
2022-01-29 19:14:33 +0000

    updated latest files

---
+++ loopy-LH.js
@@ -0,0 +1,17 @@
+let message = "";
+const loopyOn3 = "Loopy";
+const lighthouseOn4 = "Lighthouse";
+for ( let i = 100; i <= 200; i++) {
+  if (i % 3 === 0){
+    message = loopyOn3;
+  }
+  if (i % 4 === 0){
+    message += lighthouseOn4;
+  }
+  if (!message) {
+    message = i;
+  }
+  console.log(message);
+  message = "";
+}
+console.log("The end");
